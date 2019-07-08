98133a23c09595157bb32461ae2734a3067caa65
Sung Lee <sungsoolee2@hotmail.com>
2019-07-08 18:47:57 -0500

    second draft

---
+++ app/data/friends.js
@@ -0,0 +1,698 @@
+// data array to hold friends//
+
+var friendsArray = [{
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+{
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+    ]
+},
+{
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+    ]
+},
+{
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+    ]
+},
+{
+    "name": "Lou Ritter",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    "scores": [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+    ]
+},
+{
+    "name": "Jordan Biason",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+    "scores": [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+    ]
+},
+{
+    "name": "me",
+    "photo": "12",
+    "scores": [
+        "3",
+        "3",
+        "5",
+        "4",
+        "1",
+        "3",
+        "2",
+        "2",
+        "5",
+        "3"
+    ]
+},
+{
+    "name": "me",
+    "photo": "12",
+    "scores": [
+        "3",
+        "3",
+        "5",
+        "4",
+        "1",
+        "3",
+        "2",
+        "2",
+        "5",
+        "3"
+    ]
+},
+{
+    "name": "me",
+    "photo": "12",
+    "scores": [
+        "3",
+        "3",
+        "5",
+        "4",
+        "1",
+        "3",
+        "2",
+        "2",
+        "5",
+        "3"
+    ]
+},
+{
+    "name": "me",
+    "photo": "12",
+    "scores": [
+        "3",
+        "3",
+        "5",
+        "4",
+        "1",
+        "3",
+        "5",
+        "2",
+        "5",
+        "3"
+    ]
+},
+{
+    "name": "GiantsFan555",
+    "photo": "https://via.placeholder.com/150",
+    "scores": [
+        "5",
+        "4",
+        "4",
+        "2",
+        "2",
+        "4",
+        "3",
+        "2",
+        "2",
+        "1"
+    ]
+},
+{
+    "name": "far",
+    "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--_iU5hnjV--/c_scale,f_auto,fl_progressive,q_80,w_800/naiqjp2jbpvcylp09utj.png",
+    "scores": [
+        "2",
+        "5",
+        "1",
+        "3",
+        "4",
+        "2",
+        "3",
+        "3",
+        "3",
+        "2"
+    ]
+},
+{
+    "name": "aff",
+    "photo": "adf",
+    "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "2"
+    ]
+},
+{
+    "name": "aff",
+    "photo": "adf",
+    "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "3",
+        "2"
+    ]
+},
+{
+    "name": "aff",
+    "photo": "adf",
+    "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "4",
+        "3",
+        "2"
+    ]
+},
+{
+    "name": "aff",
+    "photo": "adf",
+    "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "4",
+        "4",
+        "3",
+        "2"
+    ]
+},
+{
+    "name": "aff",
+    "photo": "adf",
+    "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "5",
+        "4",
+        "4",
+        "3",
+        "2"
+    ]
+},
+{
+    "name": "aff",
+    "photo": "adf",
+    "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "5",
+        "4",
+        "4",
+        "3",
+        "2"
+    ]
+},
+{
+    "name": "Meow",
+    "photo": "11",
+    "scores": [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5"
+    ]
+},
+{
+    "name": "Pablo Diaz Trapote",
+    "photo": "https://www.google.com/search?q=alpaca&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi5qOPWv6XjAhUXK80KHcOvBU8Q_AUIECgB&biw=1440&bih=788&dpr=2#imgrc=_qs5Yrk_FlpprM:",
+    "scores": [
+        "4",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3",
+        "3"
+    ]
+},
+{
+    "name": "mitch",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Mitch_McConnell_close-up.JPG/440px-Mitch_McConnell_close-up.JPG",
+    "scores": [
+        "5",
+        "5",
+        "5",
+        "1",
+        "1",
+        "5",
+        "1",
+        "5",
+        "1",
+        "1"
+    ]
+},
+{
+    "name": "jfjjv",
+    "photo": "ejfenj",
+    "scores": [
+        "2",
+        "3",
+        "2",
+        "3",
+        "3",
+        "3",
+        "3",
+        "4",
+        "4",
+        "3"
+    ]
+},
+{
+    "name": "rew",
+    "photo": "ereq",
+    "scores": [
+        "3",
+        "2",
+        "4",
+        "5",
+        "1",
+        "1",
+        "3",
+        "2",
+        "4",
+        "1"
+    ]
+},
+{
+    "name": "Brooks",
+    "photo": "x",
+    "scores": [
+        "1",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4",
+        "4"
+    ]
+},
+{
+    "name": "Random",
+    "photo": "Random",
+    "scores": [
+        "3",
+        "5",
+        "1",
+        "4",
+        "2",
+        "4",
+        "4",
+        "3",
+        "3",
+        "3"
+    ]
+},
+{
+    "name": "amanda lowell",
+    "photo": "kajdfakjd",
+    "scores": [
+        "4",
+        "3",
+        "3",
+        "3",
+        "5",
+        "2",
+        "3",
+        "2",
+        "3",
+        "2"
+    ]
+},
+{
+    "name": "Laura Neff",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiyiMnR-qXjAhWBmeAKHcoUCJkQjRx6BAgBEAU&url=https%3A%2F%2Fwww.organicfacts.net%2Frecipe%2Fcarrot-juice&psig=AOvVaw0KVVVzZMmOaL99yJgx3R38&ust=1562697139977871",
+    "scores": [
+        "5",
+        "2",
+        "3",
+        "1",
+        "1",
+        "1",
+        "3",
+        "1",
+        "2",
+        "5"
+    ]
+},
+{
+    "name": "raj",
+    "photo": "s",
+    "scores": [
+        "2",
+        "2",
+        "2",
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "1",
+        "2"
+    ]
+},
+{
+    "name": "Dan",
+    "photo": "https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg",
+    "scores": [
+        "5",
+        "3",
+        "4",
+        "2",
+        "4",
+        "2",
+        "4",
+        "5",
+        "4",
+        "3"
+    ]
+},
+{
+    "name": "James Bowel",
+    "photo": "https://www.cut.com/people",
+    "scores": [
+        "4",
+        "4",
+        "3",
+        "3",
+        "3",
+        "3",
+        "5",
+        "4",
+        "5",
+        "5"
+    ]
+},
+{
+    "name": "James Bowel",
+    "photo": "https://www.cut.com/people",
+    "scores": [
+        "4",
+        "4",
+        "3",
+        "3",
+        "3",
+        "3",
+        "5",
+        "4",
+        "5",
+        "5"
+    ]
+},
+{
+    "name": "James Bowel",
+    "photo": "https://www.cut.com/people",
+    "scores": [
+        "4",
+        "4",
+        "3",
+        "3",
+        "3",
+        "3",
+        "5",
+        "4",
+        "5",
+        "5"
+    ]
+},
+{
+    "name": "James Bowel",
+    "photo": "https://www.cut.com/people",
+    "scores": [
+        "4",
+        "4",
+        "3",
+        "3",
+        "3",
+        "3",
+        "5",
+        "4",
+        "5",
+        "5"
+    ]
+},
+{
+    "name": "helan",
+    "photo": "H:\\Helan\\Helan_portfolio\\images",
+    "scores": [
+        "3",
+        "3",
+        "3",
+        "3",
+        "4",
+        "1",
+        "2",
+        "2",
+        "2",
+        "4"
+    ]
+},
+{
+    "name": "Lizard Man",
+    "photo": "https://y.yarn.co/025f3112-a542-4e60-aa9e-55788256af7d_screenshot.jpg",
+    "scores": [
+        "3",
+        "3",
+        "4",
+        "4",
+        "3",
+        "3",
+        "2",
+        "1",
+        "2",
+        "4"
+    ]
+},
+{
+    "name": "Lizard Man",
+    "photo": "https://y.yarn.co/025f3112-a542-4e60-aa9e-55788256af7d_screenshot.jpg",
+    "scores": [
+        "3",
+        "3",
+        "4",
+        "4",
+        "3",
+        "3",
+        "2",
+        "1",
+        "2",
+        "3"
+    ]
+},
+{
+    "name": "Bob",
+    "photo": "sdsd",
+    "scores": [
+        "3",
+        "2",
+        "2",
+        "4",
+        "2",
+        "3",
+        "3",
+        "4",
+        "3",
+        "3"
+    ]
+},
+{
+    "name": " Camerone Stoney",
+    "photo": "http://www.theotcreporter.com/wp-content/uploads/2017/11/ALRT-iStock-610041376-640x427.jpg",
+    "scores": [
+        "4",
+        "3",
+        "4",
+        "2",
+        "2",
+        "4",
+        "3",
+        "4",
+        "2",
+        "4"
+    ]
+},
+{
+    "name": "t",
+    "photo": "dasd",
+    "scores": [
+        "3",
+        "2",
+        "2",
+        "4",
+        "2",
+        "2",
+        "2",
+        "3",
+        "3",
+        "1"
+    ]
+},
+{
+    "name": "zXZX",
+    "photo": "021301302w.com",
+    "scores": [
+        "2",
+        "3",
+        "3",
+        "2",
+        "5",
+        "3",
+        "5",
+        "4",
+        "2",
+        "3"
+    ]
+},
+{
+    "name": "ryley",
+    "photo": "https://avatars1.githubusercontent.com/u/50000094?s=400&u=1b4926681b7e87861f5c9133a99370d3f00966cb&v=4",
+    "scores": [
+        "5",
+        "2",
+        "2",
+        "4",
+        "3",
+        "3",
+        "3",
+        "5",
+        "2",
+        "4"
+    ]
+},
+{
+    "name": "Yes",
+    "photo": "https://giphy.com/gifs/blizzard-heroes-of-the-storm-hanzo-1gXJgvaJELhALScz1X",
+    "scores": [
+        "2",
+        "1",
+        "4",
+        "1",
+        "3",
+        "2",
+        "3",
+        "1",
+        "1",
+        "1"
+    ]
+},
+{
+    "name": "ssk",
+    "photo": "ssksklakal",
+    "scores": [
+        "3",
+        "2",
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "3",
+        "4",
+        "2"
+    ]
+}
+]
+
+
+
+
+
+//export the array//
+module.exports = friendsArray;
